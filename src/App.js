028dc7e5036a60c299534a3978eb5113056c98b5
Sree Teja <psteja02@gmail.com>
2022-01-26 22:40:37 +0530

    404 Page added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import React, { Component } from "react";
 
 import LandingPage from "./containers/landingPage";
-import Topbar from "./components/inApp/Topbar";
+import My404Page from "./containers/404page";
 import Dashboard from "./containers/inApp/dashboard";
 import InvitePage from "./containers/inApp/invitePage";
 
@@ -19,13 +19,13 @@ function App() {
             component={() => <LandingPage history={history} />}
           />
           <Route exact path="/dashboard" component={Dashboard} />
-          {/* <Route exact path="/Topbar" component={Topbar} /> */}
           <Route
             exact
             path="/invite/:groupid"
             component={InvitePage}
             history={history}
           />
+          <Route exact path="*" component={My404Page} />
         </Switch>
       </BrowserRouter>
     </div>
